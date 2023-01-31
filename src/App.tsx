fc5179c36d59fcd2730c22ae76e42174e32dbdd6
Raymond Chen <raychen358@gmail.com>
2023-01-30 22:19:35 -0800

    Main page slider for shows

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
 import Home from "./Components/LandingPage/Home"
-import { Routes, Route,BrowserRouter as Router } from "react-router-dom"
+import { Routes, Route, BrowserRouter as Router, useNavigate, useMatch, Navigate } from "react-router-dom"
 import SignIn from "./Components/SignInPage/SignIn"
 import SignUp from "./Components/SignUpPage"
+import { useStateValue } from "./Components/State"
+import { ReactElement, useEffect, useState } from "react"
+import { Tab } from "./Services/types"
+import { userService } from "./Services/UserService"
+import { Main } from "./Components/SliderPage"
+
+const Auth = ({ children }: { children: ReactElement }) => {
+  const [{ user }] = useStateValue();
+
+  if (!user) {
+    return <Navigate replace to="/login" />;
+  }
+
+  return children;
+};
 
 const App = () => {
+
+  const [{ user }, dispatch] = useStateValue();
+  const [tab, setTab] = useState<Tab>('home')
+  const navigate = useNavigate();
+  const match = useMatch<'tab', string>('browse/:tab')
+
+  useEffect(() => {
+    const storedUser = window.localStorage.getItem('netflix-clone-user')
+    const storedUserList = window.localStorage.getItem('netflix-clone-user-list')
+
+    if (storedUser && storedUserList) {
+      const user = JSON.parse(storedUser);
+      const list = JSON.parse(storedUserList)
+      userService.setToken({ ...user, list })
+      navigate('browse')
+    }
+  }, [])
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='sign-in' element={<SignIn/>}/>
-        <Route path='sign-up' element={<SignUp/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='sign-in' element={<SignIn />} />
+        <Route path='sign-up' element={<SignUp />} />
+        <Route path='/browse/:tab' element={<Auth><Main tab={tab} />
+        </Auth>} />
       </Routes>
     </Router>
   )
---
+++ src/Components/SliderPage/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import tmdbAPI from "../../Services/tmdbAPI";
+import { Tab } from "../../Services/types";
+import { ShowByFilter, useStateValue } from "../State";
+import { Show } from "../State/types";
+import { rearrangeShowByFilter } from "../utils/helpers";
+
+
+interface SliderProps {
+    tab: Tab;
+}
+
+export const Main = ({ tab }: SliderProps) => {
+    const [{ tvshowData, movieData, homeData}, dispatch] = useStateValue();
+    const [displayContent, setDisplayContent] = useState<ShowByFilter[]>([]);
+    const [billboard, setBillboard] = useState<Show | null>(null);
+    const [Scrolled, SetScrolled] = useState<boolean>(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            SetScrolled(window.scrollY > 0)
+        });
+    });
+
+    useEffect(() => {
+        window.scrollTo(0,0);
+
+        if(tab === 'tvshows') setDisplayContent(rearrangeShowByFilter(tvshowData))
+        if(tab === 'movies') setDisplayContent(rearrangeShowByFilter(movieData))
+        if(tab === 'home') setDisplayContent(rearrangeShowByFilter(homeData))
+
+        tmdbAPI
+        .getShowForBillboard(tab)
+        .then((data: Show | null) => setBillboard(data));
+    }, [tab, tvshowData, movieData, homeData])
+    return (
+        <div className=" bg-[#141414] text-white">
+        </div>
+    )
+}
