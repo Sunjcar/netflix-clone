b77e5c9216bbb255412a1185bf2e3e7e80a60b1a
Raymond Chen <raychen358@gmail.com>
2023-02-11 22:58:45 -0800

    Updated SignUp with custom textfield hook

--- src/Components/SignUpPage/index.tsx
+++ src/Components/SignUpPage/index.tsx
@@ -1,44 +1,186 @@
-import { Link } from 'react-router-dom'
-import Netflix from '../LandingPage/Images/Netflix.png'
+import { AxiosError } from 'axios';
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { userService } from '../../Services/UserService';
+import { devices } from '../LandingPage/FAQ';
+import { TextField, useTextField } from '../SignInPage/types';
+import { useStateValue } from '../State';
+import { setUser } from '../State/reducer';
+import { signIn } from '../utils/helpers';
+import styled from "styled-components";
+
+import { NavBar, SignIn } from '../Navbar';
+import { MdWarning } from 'react-icons/md';
 
 const SignUp = () => {
-    return (
-        <div className='w-[100vw] h-[100vh]'>
-            <header className='z-10 flex items-center justify-between p-8 '>
-                <Link to='/'>
-                <img className='w-[9rem] z-10' src={Netflix} />
-                </Link>
-                <Link to='/sign-in'>
-                    <button className='z-10 px-5 py-2 text-xl text-black font-[500] '> Sign In </button>
-                </Link>
-            </header>
-            <hr></hr>
-            <section className='relative rounded-lg flex box-border flex-col px-[60px] pt-[68px] pb-[40px] text-white max-w-[500px] min-h-[660px] mx-auto z-10 '>
-                <div>
-                    <h1 className='text-black text-[1.8em] font-[400] mt-[.67em] w-full'>Create a password to start your membership</h1>
-                    <h3 className='text-black'>Just a few more steps and you're done!</h3>
-                    <h3 className='text-black'> We hate paperwork, too </h3>
-                </div>
-                <div>
-                    <form>
-                        <div className='relative flex flex-col pb-[16px] mt-[10px]'>
-                            <input type='email' className=' leading-5 rounded-sm border-[#8c8c8c] border-[1px] text-black px-[16px] pt-[24px] pb-1 peer' placeholder=" " />
-                            <label className=' text-[#8c8c8c] pointer-events-none text-[14px] absolute duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4'> Email or Phone Number</label>
-                        </div>
-                        <div className='relative flex flex-col pb-[16px]'>
-                            <input type='password' className=' leading-5 rounded-sm border-[#8c8c8c] border-[1px] text-black px-[16px] pt-[24px] pb-1 peer' placeholder=' ' />
-                            <label className=' text-[#8c8c8c] pointer-events-none text-[14px] absolute duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4'> Password</label>
-                        </div>
-                        <button className='z-10 p-[12px] rounded-sm text-white bg-[#e50914] mt-[6px] mb-[12px] hover:bg-red-500 w-full text-[1rem]'>
-                            Sign Up
-                        </button>
-                    </form>
-                </div>
-            </section>
-        </div>
-    )
-}
 
-export default SignUp
+  const [errMsg, setErrMsg] = useState<string>("");
+  const [, dispatch] = useStateValue();
+  const email = useTextField("Email", "signup", "account");
+  const password = useTextField("Add a password", "signup", "password");
+  const navigate = useNavigate();
+
+  const handleSignUp = async (event: FormEvent) => {
+    event.preventDefault();
+    if (!email.value || !password.value) {
+      email.handleEmptySubmit();
+      password.handleEmptySubmit();
+      return;
+    }
+    if (email.errMsg || password.errMsg) return;
+
+    try {
+      const newUser = await userService.createUser({
+        username: email.value,
+        password: password.value,
+      });
+      dispatch(setUser(newUser));
+      signIn(newUser);
+      navigate("/browse");
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        if (err.response?.data.error) {
+          const error = err.response?.data.error as string;
+          if (error.includes("unique"))
+            setErrMsg(
+              "Looks like that account already exists. Sign into that account or try using a different email."
+            );
+          else setErrMsg(error);
+        }
+      }
+    }
+
+  };
+  return (
+    <SignUpPageContainer>
+      <NavBar className="signup-page">
+        <SignIn to="/login" className="signup">
+          Sign In
+        </SignIn>
+      </NavBar>
+      <SignUpFormContainer>
+        <SignUpForm>
+          {errMsg ? (
+            <div className="error-message">
+              <div>
+                <MdWarning />
+              </div>
+              <h4>{errMsg}</h4>
+            </div>
+          ) : null}
+          <h1>Create a password to start your membership</h1>
+          <h3>Just a few more steps and you&apos;re done!</h3>
+          <h3>We hate paperwork, too</h3>
+          <FormField>
+            <TextField {...email} />
+            <TextField {...password} />
+            <RedButton onClick={handleSignUp} className="signup">
+              Sign Up
+            </RedButton>
+          </FormField>
+        </SignUpForm>
+      </SignUpFormContainer>
+    </SignUpPageContainer>
+  );
+};
+
+export default SignUp;
 
 
+export const SignUpPageContainer = styled.div`
+  background-color: white;
+  color: #333;
+`;
+
+export const SignUpFormContainer = styled.div`
+  border-top: 1px solid #e6e6e6;
+  margin-top: 20px;
+`;
+
+export const SignUpForm = styled.div`
+  position: relative;
+  background-color: transparent;
+  border-radius: 4px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  padding: 30px 50px 40px;
+  font-family: "Netflix Sans Light";
+  color: #333;
+  max-width: 500px;
+  min-height: 660px;
+  margin: 0 auto;
+  z-index: 10;
+  @media ${devices.medium} {
+    padding: 15px 25px 20px;
+  }
+  div.error-message {
+    padding: 20px 10px;
+    background-color: rgb(251, 164, 4);
+    display: flex;
+    align-items: center;
+    column-gap: 1rem;
+    @media ${devices.medium} {
+      padding: 10px 5px;
+      svg {
+        font-size: 1rem;
+      }
+    }
+    h4 {
+      margin: 0;
+    }
+    svg {
+      font-size: 1.5rem;
+    }
+  }
+  h1 {
+    font-family: "Netflix Sans";
+    font-weight: 400;
+  }
+  h3 {
+    margin: 0 0 16px;
+  }
+`;
+
+export const FormField = styled.div`
+  flex: 1;
+  input {
+    border: 1px solid #8c8c8c;
+  }
+  button {
+    border: none;
+    border-radius: 4px;
+  }
+`;
+
+export const RedButton = styled.button`
+  background-color: rgb(229, 9, 20);
+  background-image: linear-gradient(180deg, #e50914, #db0510);
+  color: white;
+  border: none;
+  font-family: "Netflix Sans Light";
+  height: 60px;
+  width: 250px;
+  font-size: 1.625rem;
+  border: 1px solid #333;
+  cursor: pointer;
+  @media ${devices.large} {
+    width: 140px;
+    height: 48px;
+    font-size: 1rem;
+    border-radius: 4px;
+  }
+  @media ${devices.medium} {
+    width: 120px;
+    height: 40px;
+    font-size: 0.8rem;
+  }
+  &.signin {
+    width: 100%;
+    font-size: 1rem;
+  }
+  &.signup {
+    width: 100%;
+    font-size: 1rem;
+  }
+`;
