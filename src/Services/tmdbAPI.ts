c6df77e90b9c7bf0bfbb162c57cb702387ee47b9
Raymond Chen <raychen358@gmail.com>
2023-03-07 21:44:28 -0800

    Fixed image url for billboard

--- src/Services/tmdbAPI.ts
+++ src/Services/tmdbAPI.ts
@@ -13,7 +13,8 @@ import {
 } from "./types";
 
 export const BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY = process.env.REACT_APP_TMDB_APIKEY;
+export const API_KEY = process.env.REACT_APP_TMDB_APIKEY;
+
 
 const tabToType = (tab: Tab): MediaType => {
   return tab === "home"
