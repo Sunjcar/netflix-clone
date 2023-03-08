0cc9fdc82cf074029a2af7ec82a213c2808672df
Raymond Chen <raychen358@gmail.com>
2023-03-07 20:38:14 -0800

    included api to services, login works

--- src/Services/UserService.ts
+++ src/Services/UserService.ts
@@ -1,39 +1,37 @@
 import axios from "axios";
-import { BASE_URL } from "./tmdbAPI";
-
+import { API_KEY, BASE_URL } from "./tmdbAPI";
 import { Show, User } from "./types";
 
-interface userAccess {
-    username: string;
-    password: string;
+
+
+interface userRequest {
+  username: string;
+  password: string;
 }
 
 let token: string;
 
-const createUser = async (newUser: userAccess): Promise<User> => {
-    const res = await axios.post(BASE_URL, newUser);
-    console.log(res)
-    return res.data;
+const createUser = async (newUser: userRequest): Promise<User> => {
+  const response = await axios.post(`${BASE_URL}${API_KEY}`, newUser);
+  return response.data;
 };
 
 const checkUser = async (username: string): Promise<boolean> => {
-    const res = await axios.post(`${BASE_URL}/check`, { username });
-    console.log(res.data)
-    return res.data;
+  const response = await axios.post(`${BASE_URL}${API_KEY}/check`, { username });
+  return response.data;
 };
 
 const setToken = (newToken: string) => {
-    token = `tok ${newToken}`;
+  token = `tok ${newToken}`;
 };
 
-const toggleList = async (input: Show): Promise<{ newList: Show[]; }> => {
-    const config = {
-        headers: { Authorization: token }
-    };
+const toggleList = async (input: Show): Promise<{ newList: Show[] }> => {
+  const config = {
+    headers: { Authorization: token },
+  };
 
-    const res = await axios.put(BASE_URL, { showToToggle: input }, config);
-    console.log(res)
-    return res.data;
+  const res = await axios.put(`${BASE_URL}${API_KEY}`, { showToToggle: input }, config);
+  return res.data;
 };
 
 
@@ -45,10 +43,8 @@ interface Credentials {
 }
 
 const login = async (credentials: Credentials): Promise<User> => {
-    const response = await axios.post<User>(BASE_URL, credentials);
-    console.log(response)
-    console.log(response.data);
-    return (response.data);
+    const res = await axios.post<User>(`${BASE_URL}${API_KEY}`, credentials);
+    return (res.config.data);
 };
 
 export const userService = {
