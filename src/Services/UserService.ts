3cdfaed4f512e0da7584177421dd86ca38984e33
Raymond Chen <raychen358@gmail.com>
2023-03-14 21:49:33 -0700

    return correct data for mylist

--- src/Services/UserService.ts
+++ src/Services/UserService.ts
@@ -25,32 +25,32 @@ const setToken = (newToken: string) => {
   token = `tok ${newToken}`;
 };
 
-const toggleList = async (input: Show): Promise<{ newList: Show[] }> => {
+const toggleList = async (input: Show): Promise<{ newList: Show[]; }> => {
   const config = {
     headers: { Authorization: token },
   };
 
   const res = await axios.put(`${BASE_URL}${API_KEY}`, { showToToggle: input }, config);
-  return res.data;
+  return res.config.data;
 };
 
 
 
 
 interface Credentials {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 const login = async (credentials: Credentials): Promise<User> => {
-    const res = await axios.post<User>(`${BASE_URL}${API_KEY}`, credentials);
-    return (res.config.data);
+  const res = await axios.post<User>(`${BASE_URL}${API_KEY}`, credentials);
+  return (res.config.data);
 };
 
 export const userService = {
-    createUser,
-    checkUser,
-    setToken,
-    toggleList,
-    login,
+  createUser,
+  checkUser,
+  setToken,
+  toggleList,
+  login,
 };
